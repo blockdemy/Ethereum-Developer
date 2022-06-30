6954d129f8445c691a1dd88e621377c1f4b8e732
lizduran <42351387+lizduran@users.noreply.github.com>
2022-06-29 22:25:12 -0500

    ERC20 con Open Zeppelin

--- OZ-ERC20.sol
+++ OZ-ERC20.sol
@@ -1,13 +1,16 @@
+// contracts/ERC20OZ.sol
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.2;
+
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ERC20OpenZ is ERC20{
-    constructor() ERC20 ("LizToken", "LTK"){
-        //Indicar total supply
-        _mint(msg.sender, 1000*10**decimals());
+contract ERC20OZ is ERC20 {
+    constructor(uint256 initialSupply) ERC20("LizToken", "LTK") {
+        _mint(msg.sender, initialSupply*10**decimals());
     }
 }
+
+
 
