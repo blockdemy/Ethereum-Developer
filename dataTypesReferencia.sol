c2a6274715f5cbb140872e6eb5f2326f8649e6d3
Liz Durán <42351387+liiz-durang@users.noreply.github.com>
2022-08-17 22:09:54 -0500

    Tipos de Referencia en Solidity

--- dataTypesReferencia.sol
+++ dataTypesReferencia.sol
@@ -1,34 +1,36 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity 0.8.0;
-
-contract dataTypesReferencia{
+pragma solidity ^0.8.0;
 
 
-    //Struct
+contract dataTypeReferencia{
+
+    //Struct 
+
     struct Persona{
-        bool vacunado;
+        bool vacunado; 
         uint8 edad;
-        bytes32 nombre;
+        bytes32 nombre; 
     }
 
-    Persona public p1 = Persona(true, 32, "Lizeth");
+    Persona public p1 = Persona(true, 30, "Lizeth");
+
     Persona public p2 = Persona({
         edad: 30,
-        vacunado: true, 
-        nombre: "Lizeth"});
+        vacunado: false,
+        nombre: "Luis"
+    });
 
-    uint8 public edadP1 = p1.edad;
+    uint8 public getEdad = p1.edad;
 
-    /*****************************************************************/
-    
-    //Mapping - mapping(_KeyType => _ValueType)
+    /**************/
+    //Mapping - mapping(_KeyType => _ValueType) [keyTipe] = valueType
 
-    mapping(address => Persona) diccPersonas; 
+    mapping(address => Persona) public diccPersona; 
 
-    function addPersona() public{
-        diccPersonas[msg.sender] = p1;
+    function addPersona() public {
+        diccPersona[msg.sender] = p1;
     }
-    
+
 
 }
