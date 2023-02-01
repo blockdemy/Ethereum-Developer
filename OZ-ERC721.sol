948c0cbb377bef0c24a6ce59090cc6c3c568eeac
Liz Durán <42351387+liiz-durang@users.noreply.github.com>
2023-02-01 16:32:27 -0600

    Update OZ-ERC721.sol

--- OZ-ERC721.sol
+++ OZ-ERC721.sol
@@ -2,15 +2,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-
-contract GameItem is ERC721URIStorage {
+contract MyToken  is ERC721URIStorage  {
 
     uint256 private _counter;
-    constructor() ERC721("GameItem", "ITM") {
-        _counter = 0;
-    }
+     constructor() ERC721("MyToken", "MTK") {
+         _counter = 0;
+     }
 
     function mint (address _receiver, string memory _tokenURI) public {
         require(_counter < 1000, "Minted All"); 
