3b2ab269d4287b641c2ae38a62a91eafa7ff1a3b
Liz Durán <42351387+liiz-durang@users.noreply.github.com>
2023-02-03 17:54:12 -0600

    Update getEthPrice.sol

--- oraculo/getEthPrice.sol
+++ oraculo/getEthPrice.sol
@@ -1,42 +1,33 @@
-
-
-
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity ^0.8.7;
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract getEthPrice {
+contract PriceConsumerV3 {
     AggregatorV3Interface internal priceFeed;
-     address constant priceAddress = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e ;
+
     /**
-     * Network: RINKEBY
+     * Network: Goerli
      * Aggregator: ETH/USD
-     * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
      */
     constructor() {
-        priceFeed = AggregatorV3Interface(priceAddress);
+        priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
     }
 
-    function getLatestEthPrice() public view returns( uint80 , int256 , uint256 , uint256 , uint80 ){
+    /**
+     * Returns the latest price.
+     */
+    function getLatestPrice() public view returns (int) {
         (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
             uint80 answeredInRound
         ) = priceFeed.latestRoundData();
-        return (  roundId,  answer,  startedAt, updatedAt, answeredInRound);
-    } 
-
-    function getOnlyEthPrice() public view returns( int256 ){
-        (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
-        return answer;
-    } 
-
+        return price;
+    }
 }
