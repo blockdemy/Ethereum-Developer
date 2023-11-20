e1ee7d92fa46cbc995c89435b01affec7e6f4bb4
Liz Durán <42351387+liiz-durang@users.noreply.github.com>
2023-11-20 00:11:15 -0600

    Update firstSmartContract.sol

--- firstSmartContract.sol
+++ firstSmartContract.sol
@@ -1,31 +1,14 @@
-// SPDX-License-Identifier: GPL-3.0 
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
 
-//version
-pragma solidity >=0.7.0 <0.9.0;
+contract SimpleStorage {
+    uint storedData;
 
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
-contract Storage {
-
-    uint256 number;
-
-
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function store(uint256 num) public {
-        number = num;
+    function set(uint x) public {
+        storedData = x;
     }
 
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256){
-        return number;
+    function get() public view returns (uint) {
+        return storedData;
     }
 }
