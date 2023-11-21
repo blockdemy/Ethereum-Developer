3ea50f48fe1a53ade90204565b9de4fd7239a775
Liz Durán <42351387+liiz-durang@users.noreply.github.com>
2023-11-20 21:06:59 -0600

    Update TransferirEther.sol

--- TransferirEther.sol
+++ TransferirEther.sol
@@ -4,19 +4,28 @@ pragma solidity ^0.8.0;
 
 
 contract SendEther{
-    //transfer()
+
+     //send() - Ya no se usa. No revierte la transacción. Para eso se usa require 
+     //Permite 2300 unidades de gas
+    function sendEtherSend(address payable _to) public payable{
+        bool sent = _to.send(msg.value);
+        require (sent, "Error");
+    }
+
+
+    //transfer() - Si no se puede, se revierte la transacción
+    //Permite 2300 unidades de gas
     function sendEtherTransfer(address payable _to) public payable{
         _to.transfer(msg.value);
     }
     
-    //send()
-    function sendEtherSend(address payable _to) public payable returns(bool sent){
-        sent = _to.send(msg.value);
-    }
-
+   
     //call()
-    function sendEtherCall(address payable _to) public payable returns(bool sent, bytes memory data){
-        (sent,data) = _to.call{value: msg.value}("");
+    //Permite todo el gas displonible. pPsibilidad de un retorno de datos desde la función receptor.
+    function sendEtherCall(address payable _to) public payable returns (bytes memory) {
+        (bool sent,  bytes memory data ) = _to.call{value: msg.value, gas: 10000}("");
+        require (sent, "Error");
+        return data;
 
     }
 }
@@ -37,5 +46,5 @@ contract RecibirEther{
     fallback() external payable{
 
     }
-    //getBalance()
+    
 }
